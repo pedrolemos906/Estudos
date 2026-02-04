5ac4bcbb4390fd5279a27bf457047e713290d66d
Pedro Lemos <pedrolemos906@gmail.com>
2026-02-04 19:20:51 -0300

    commit do dia 04/02/2026

---
+++ Python/Curso em Vídeo/Mundo 3/Desafio 2.py
@@ -0,0 +1,11 @@
+"""Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Brasileirão, na ordem de colocação.
+Depois mostre:
+a) os 5 primeiros.
+b) os 4 ultimos.
+c) os times em ordem alfabetica.
+d) em qual posiçao esta o time da chapecoense."""
+
+times = ('botafogo', 'chapecoense', 'vitoria', 'fluminense', 'mirassol',
+        'bahia', 'sao paulo', 'athetico', 'bragantino', 'palmeiras', 'atletico - mg',
+        'flamengo', 'gremio', 'corinthians', 'vasco', 'coritiba', 'internacional', 'santos', 'remo', 'cruzeiro')
+        
